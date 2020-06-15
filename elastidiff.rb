5c136b40d28040f1879d0a1de5337aec046bf979
Jeremiah Via <jmv@gamayun.io>
2020-06-14 20:27:43 -0700

    Upgrade to elastidiff:0.2.0

--- elastidiff.rb
+++ elastidiff.rb
@@ -1,14 +1,14 @@
 class Elastidiff < Formula
   desc "Elasticsearch index diffing tool."
   homepage "https://github.com/jvia/elastidiff"
-  version "0.1.0"
+  version "0.2.0"
 
   if OS.linux?
-    url "https://github.com/jvia/elastidiff/releases/download/0.1.0/elastidiff-linux-amd64.zip"
-    sha256 "05d61e95cd7233fdebc85517ca00f4b89580f5c1b2fa7c8a74615b2727b0286d"
+    url "https://github.com/jvia/elastidiff/releases/download/0.2.0/elastidiff-linux-amd64.zip"
+    sha256 "c91c2826016587cddd10df60c6ea4d9067d3b2643c4d418e0823556ff313abb0"
   else
-    url "https://github.com/jvia/elastidiff/releases/download/0.1.0/elastidiff-macos-amd64.zip"
-    sha256 "8852ed0d63ced4370c005d2b2c027cd61574c57ee4d8b6c724b9ef2252afcf1f"
+    url "https://github.com/jvia/elastidiff/releases/download/0.2.0/elastidiff-macos-amd64.zip"
+    sha256 "568f946d67d0226f029b79c0ab4acece573265c5c7debef1a47ad0fa22151376"
   end
 
   bottle :unneeded
